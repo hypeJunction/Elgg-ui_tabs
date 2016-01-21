48643212d9888af9ee0ce253cc2e3accd5afe003
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2016-01-21 20:11:33 +0100

    fix(releases): fix automated releases

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,7 +20,7 @@ module.exports = function (grunt) {
 		},
 		clean: {
 			release: {
-				src: ['build/', 'releases/', 'vendor/', 'composer.lock']
+				src: ['build/', 'releases/', 'mod/', 'vendor/', 'composer.lock']
 			}
 		},
 		copy: {
@@ -30,6 +30,7 @@ module.exports = function (grunt) {
 					'!**/.git*',
 					'!releases/**',
 					'!build/**',
+					'!mod/**',
 					'!node_modules/**',
 					'!package.json',
 					'!config.rb',
@@ -68,9 +69,27 @@ module.exports = function (grunt) {
 				}
 			},
 		},
+		gitfetch: {
+			release: {
+				all: true
+			}
+		},
+		gittag: {
+			release: {
+				options: {
+					tag: '<%= pkg.version %>',
+					message: 'Release <%= pkg.version %>'
+				}
+			}
+		},
 		gitpush: {
 			release: {
 			},
+			release_tags: {
+				options: {
+					tags: true
+				}
+			}
 		},
 		gh_release: {
 			options: {
@@ -80,7 +99,6 @@ module.exports = function (grunt) {
 			},
 			release: {
 				tag_name: '<%= pkg.version %>',
-				target_commitish: '1.x',
 				name: 'Release <%= pkg.version %>',
 				body: grunt.file.read('release.md'),
 				draft: false,
@@ -97,11 +115,11 @@ module.exports = function (grunt) {
 				changelogOpts: {
 					// conventional-changelog options go here
 					preset: 'angular'
-				},
+				}
 			},
 			release: {
 				src: 'CHANGELOG.md'
-			},
+			}
 
 		}
 	});
@@ -126,14 +144,16 @@ module.exports = function (grunt) {
 			'version::' + n,
 			'readpkg',
 			'conventionalChangelog:release',
+			'gitfetch:release',
 			'gitcommit:release',
+			'gittag:release',
 			'gitpush:release',
+			'gitpush:release_tags',
 			'clean:release',
 			'composer:install:no-dev:prefer-dist',
 			'copy:release',
 			'compress:release',
 			'gh_release',
-			'clean:release',
 		]);
 	});
 };
